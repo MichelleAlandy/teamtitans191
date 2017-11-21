a61e7734ceabf051133382fb3e2f40d90279ffe9
henrylaynesa <henry.laynesa@gmail.com>
2017-11-21 16:08:29 +0800

    added join tables

--- fria2/db/schema.rb
+++ fria2/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121074436) do
+ActiveRecord::Schema.define(version: 20171121075900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admin_accounts", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "admin_id", null: false
+    t.index ["admin_id"], name: "index_admin_accounts_on_admin_id"
+    t.index ["user_id"], name: "index_admin_accounts_on_user_id"
+  end
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
