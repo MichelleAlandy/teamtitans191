d11ae3bdfb4c22e0ec9dd7fb57673d3644e172d4
Henry Laynesa Jr <henry.laynesa@gmail.com>
2017-11-27 10:15:08 +0800

    deleted home and added landing_page (blank)

--- fria2/config/routes.rb
+++ fria2/config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :reviews
 
   
-  root :to => 'home#index'
+  root :to => 'landing_page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
