7d618b46376e6d19d1895530c3e91bcb453dcf47
Henry Laynesa Jr <henry.laynesa@gmail.com>
2017-11-27 15:34:44 +0800

    PUTANGINA AYOKO NA

--- fria2/app/controllers/landing_page_controller.rb
+++ fria2/app/controllers/landing_page_controller.rb
@@ -1,13 +1,20 @@
 class LandingPageController < ApplicationController
 	def index
 		@user ||= User.new
-		puts "--------"
-		puts current_user.curr_type
-		puts "--------"
-		puts current_user.email
-		puts "--------"
-		if current_user.curr_type == 'Dean'
+		if !@user.curr_type
+			@user.curr_type = params[:curr_type]
+		end
+		if current_user && @user.curr_type == 'Dean'
 			render 'dean/index'
+		end
+		if current_user && @user.curr_type == 'Researcher'
+			render 'researcher/index'
+		end
+		if current_user && @user.curr_type == 'Admin'
+			render 'admin/index'
+		end
+		if current_user && @user.curr_type == 'Committee Member'
+			render 'committee_member/index'
 		end
 	end
 end
---
+++ fria2/app/controllers/researcher_controller.rb
@@ -0,0 +1,4 @@
+class ResearcherController < ApplicationController
+  def index
+  end
+end
